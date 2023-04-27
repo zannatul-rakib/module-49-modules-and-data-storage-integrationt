54e0d331baaec60633a1c36d22fbd5c0089b78cb
zrakib-dev <dev.zrakib@gmail.com>
2023-04-28 00:14:08 +0600

    1st video complete

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Cosmetics from './components/Cosmetics/Cosmetics'
+
+function App() {
+  
+
+  return (
+    <>
+      <Cosmetics></Cosmetics>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Cosmetics/Cosmetics.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cosmetics = () => {
+    return (
+        <div>
+            <h2>Cosmetics</h2>
+        </div>
+    );
+};
+
+export default Cosmetics;
